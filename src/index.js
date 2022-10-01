e261948e4be0c8f22c7c4d74f3ffda24d8deed7a
Serhii <sergenadoenko@gmail.com>
2022-10-01 15:19:06 +0200

    added server error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelServise from './services/MarvelService';
+
 import './style/style.scss';
 
-
-const marvelService = new MarvelServise();
-
-marvelService.getAllCharacters().then(res=>res.data.results.forEach(item=>console.log(item.name)));
 
 
 
