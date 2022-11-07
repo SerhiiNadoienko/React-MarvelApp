339f31093a55b31525d6e2dde0766a9786a066b4
Serhii <sergenadoenko@gmail.com>
2022-11-07 15:01:12 +0100

    used Finite-state machine

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,10 +1,30 @@
-import React,  {useState, useEffect, useRef} from 'react';
+import React,  {useState, useEffect, useRef, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import Spinner from '../spinner/spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import useMarvelService from '../../services/MarvelService';
 import './charList.scss';
+
+const setContent = (process,Component,newItemLoading) => {
+    switch(process) {
+        case 'waiting':
+            return <Spinner/>;
+            break;
+        case 'loading':
+            return newItemLoading ? <Component/> : <Spinner/>;
+            break;
+        case 'confirmed':
+            return <Component/>;
+            break;
+        case 'error':
+            return <ErrorMessage/>;
+            break;
+        default:
+            throw new Error('Unexpected process state') 
+    }
+}
+
 
 const CharList = (props) => {
 
@@ -13,7 +33,7 @@ const CharList = (props) => {
     const [offset, setOffset] = useState(210);
     const [charEnded, setCharEnded] = useState(false);
     
-    const {loading, error, getAllCharacters} = useMarvelService();
+    const {loading, error, getAllCharacters, process, setProcess} = useMarvelService();
 
     useEffect(() => {
         onRequest(offset, true);
@@ -23,6 +43,7 @@ const CharList = (props) => {
         initial ? setNewItemLoading(false) : setNewItemLoading(true);
         getAllCharacters(offset)
             .then(onCharListLoaded)
+            .then(()=> setProcess('confirmed'))   
     }
 
     const onCharListLoaded =  (newCharList) => {
@@ -85,18 +106,14 @@ const CharList = (props) => {
             </ul>
         )
     }
-    
-    const items = renderItems(charList);
-
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading && !newItemLoading? <Spinner/> : null;
-
+     
+    const elements = useMemo(()=> {
+        return  setContent(process,()=> renderItems(charList), newItemLoading);
+    },[process])
 
     return (
-        <div className="char__list">
-            {errorMessage}
-            {spinner}
-            {items}
+        <div className="char__list"> 
+        {elements}
             <button 
                 className="button button__main button__long"
                 disabled={newItemLoading}
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import {useHttp} from '../hooks/http.hook';
 
 const useMarvelService = () => {
-    const {loading, request, error, clearError} = useHttp();
+    const {loading, request, error, clearError, process, setProcess } = useHttp();
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
@@ -52,7 +52,16 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic}
+    return {
+        loading, 
+        error, 
+        clearError, 
+        process, 
+        setProcess,
+        getAllCharacters, 
+        getCharacter, 
+        getAllComics, 
+        getComic}
 }
 
 export default useMarvelService;
